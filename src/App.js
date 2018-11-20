e528cad56191609809e394dadc85af62328bb079
maxyou <hyyou@foxmail.com>
2018-11-21 01:17:26 +0800

    axios get done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Pages from './scaffolds/pages'
 import Todo from './scaffolds/todo'
+import CNode from './scaffolds/cnode'
 import ContainerTodoAdd from './containers/todoadd'
 import ContainerTodoList from './containers/todolist'
 import ContainerTodoFilter from './containers/todofilter'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers';
-import { HashRouter, Route, Redirect } from 'react-router-dom'
+import { Switch, HashRouter, Route, Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 
 const store = createStore(
@@ -28,11 +29,12 @@ class App extends Component {
                 {/* <Link to="/">to /</Link>{'  '}
                 <Link to="/about">to about</Link>{'  '}
                 <Link to="/topics">to topics</Link> */}
-
-                <Route exact path="/" render={()=><Redirect to="/pages" />} />
-                <Route path="/pages" component={Pages} />
-                <Route path="/todo" component={Todo} />
-
+                <Switch>
+                  <Route exact path="/" render={()=><Redirect to="/cnode" />} />
+                  <Route path="/pages" component={Pages} />
+                  <Route path="/todo" component={Todo} />
+                  <Route path="/cnode" component={CNode} />
+                </Switch>
               {/* <ContainerTodoAdd />
               <ContainerTodoList />
               <ContainerTodoFilter /> */}
---
+++ src/scaffolds/cnode.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import axios from 'axios'
+
+const Header = styled.div`
+    background-color:grey;
+`
+
+class CNode extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+            data:{}
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/topics')
+            .then(
+                res=>{
+                    if(res.status==200){
+                        this.setState({data:res.data})
+                    }
+                    console.log(res)
+                }
+            )
+    }
+
+    render() {
+        return (
+            <div>
+                <Header>cnode</Header>
+                {JSON.stringify(this.state.data)}
+            </div>
+        )
+    }
+}
+
+export default CNode
