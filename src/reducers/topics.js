bbfd5d8afdb31fe6783fe170f611f887cf00da53
maxyou <hyyou@foxmail.com>
2018-11-28 00:00:47 +0800

    rename

---
+++ src/reducers/topics.js
@@ -0,0 +1,27 @@
+import {ACTION} from '../actions/topics'
+
+const initState = {
+    loading: false,
+    // topics:[]
+    data: {
+        data: []
+    }
+}
+
+const topics = (state = initState, action) => {
+    switch (action.type) {
+        case ACTION.HTTP_REQUEST:
+            // console.log('ACTION.HTTP_REQUEST')
+            return {...state, loading: true}
+        case ACTION.HTTP_FAILURE:
+            return {...state, loading: false}
+        case ACTION.HTTP_SUCCESS:
+            console.log('ACTION.HTTP_SUCCESS')
+            console.log(action.payload)
+            return {...state, loading: false, data: action.payload}
+        default:
+            return state
+    }
+}
+
+export default topics
